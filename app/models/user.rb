131e65acd2092dd0c606031bcc5820b0ec4b244d
lukegibson101 <luke.gibson@nourishcare.co.uk>
2022-07-04 11:06:30 +0100

    set up messages table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   validates :username, presence: true,
                        uniqueness: {case_sensitive: false},
-                       length: {minimum: 3, maximum:25}
+                       length: {minimum: 3, maximum:25},
+                       uniqueness: {case_sensitive: false}
+  has_many :messages
   has_secure_password
 end
